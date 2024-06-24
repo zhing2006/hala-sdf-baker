ec97adb2ed22545480345e06a6625f5e4ab47796
zhing2006 <382601573@qq.com>
2024-06-24 23:49:18 +0800

    Add shaders.

---
+++ shaders/src/hala-sdf-baker/test.ps_6_8.hlsl
@@ -0,0 +1,16 @@
+#include "common.hlsl"
+
+struct PSInput {
+  float4 position: SV_Position;
+  float3 color: COLOR0;
+};
+
+struct PSOutput {
+  float4 color: SV_Target0;
+};
+
+PSOutput Main(PSInput input) {
+  PSOutput output = (PSOutput)0;
+  output.color = float4(input.color, 1.0);
+  return output;
+}
