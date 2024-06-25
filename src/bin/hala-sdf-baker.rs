d369d9555a1ebf4d34a696595deceb640fd466a5
zhing2006 <382601573@qq.com>
2024-06-25 12:55:29 +0800

    Add base runtime framework.

--- src/bin/hala-sdf-baker.rs
+++ src/bin/hala-sdf-baker.rs
@@ -1,7 +1,151 @@
-use anyhow::Result;
+use anyhow::{
+  Result,
+  Context,
+};
+
+use clap::{arg, Command};
+
+use hala_sdf_baker::config;
+
+use hala_imgui::{
+  HalaApplication,
+  HalaImGui,
+};
+
+/// The SDF baker application.
+struct SDFBakerApplication {
+  log_file: String,
+  config: config::AppConfig,
+  imgui: Option<HalaImGui>,
+}
+
+/// The implementation of the SDF baker application.
+impl SDFBakerApplication {
+  pub fn new() -> Result<Self> {
+    // Parse the command line arguments.
+    let matches = cli().get_matches();
+    let log_file = match matches.get_one::<String>("log") {
+      Some(log_file) => log_file,
+      None => "./logs/sdf_baker.log"
+    };
+    let config_file = matches.get_one::<String>("config").with_context(|| "Failed to get the config file path.")?;
+
+    // Load the configure.
+    let config = config::load_app_config(config_file)?;
+    log::debug!("Config: {:?}", config);
+    config::validate_app_config(&config)?;
+
+    // Create out directory.
+    std::fs::create_dir_all("./out")
+      .with_context(|| "Failed to create the output directory: ./out")?;
+
+    Ok(Self {
+      log_file: log_file.to_string(),
+      config,
+      imgui: None,
+    })
+  }
+}
+
+/// The implementation of the application trait for the SDF baker application.
+impl HalaApplication for SDFBakerApplication {
+  fn get_log_console_fmt(&self) -> &str {
+    "{d(%H:%M:%S)} {h({l:<5})} {t:<20.20} - {m}{n}"
+  }
+  fn get_log_file_fmt(&self) -> &str {
+    "{d(%Y-%m-%d %H:%M:%S)} {h({l:<5})} {f}:{L} - {m}{n}"
+  }
+  fn get_log_file(&self) -> &std::path::Path {
+    std::path::Path::new(self.log_file.as_str())
+  }
+  fn get_log_file_size(&self) -> u64 {
+    1024 * 1024 /* 1MB */
+  }
+  fn get_log_file_roller_count(&self) -> u32 {
+    5
+  }
+
+  fn get_window_title(&self) -> &str {
+    "SDF Baker"
+  }
+  fn get_window_size(&self) -> winit::dpi::PhysicalSize<u32> {
+    winit::dpi::PhysicalSize::new(self.config.window.width as u32, self.config.window.height as u32)
+  }
+
+  fn get_imgui(&self) -> Option<&HalaImGui> {
+    self.imgui.as_ref()
+  }
+  fn get_imgui_mut(&mut self) -> Option<&mut HalaImGui> {
+    self.imgui.as_mut()
+  }
+
+  /// The before run function.
+  /// param width: The width of the window.
+  /// param height: The height of the window.
+  /// param window: The window.
+  /// return: The result.
+  fn before_run(&mut self, _width: u32, _height: u32, _window: &winit::window::Window) -> Result<()> {
+    Ok(())
+  }
+
+  /// The after run function.
+  fn after_run(&mut self) {
+    // if let Some(renderer) = &mut self.renderer.take() {
+    //   renderer.wait_idle().expect("Failed to wait the renderer idle.");
+    //   self.imgui.take();
+    // }
+  }
+
+  /// The update function.
+  /// param delta_time: The delta time.
+  /// return: The result.
+  fn update(&mut self, delta_time: f64, width: u32, height: u32) -> Result<()> {
+    if let Some(imgui) = self.imgui.as_mut() {
+      imgui.begin_frame(
+        delta_time,
+        width,
+        height,
+        |ui| {
+          ui.window("Path Tracer")
+            .position([10.0, 10.0], imgui::Condition::FirstUseEver)
+            .build(|| {
+              if ui.button_with_size("Save", [100.0, 30.0]) {
+                log::info!("Save button clicked.");
+              }
+            }
+          );
+        }
+      )?;
+      imgui.end_frame()?;
+    }
+
+    Ok(())
+  }
+
+  /// The render function.
+  /// return: The result.
+  fn render(&mut self) -> Result<()> {
+    Ok(())
+  }
+}
+
+/// The command line interface.
+fn cli() -> Command {
+  Command::new("sdf-baker")
+    .about("The SDF Baker.")
+    .arg_required_else_help(true)
+    .arg(arg!(-l --log <LOG_FILE> "The file path of the log file. Default is ./logs/sdf_baker.log."))
+    .arg(arg!(-c --config [CONFIG_FILE] "The file path of the config file."))
+}
 
 /// The normal main function.
 fn main() -> Result<()> {
-  println!("Hello, world!");
+  // Initialize the application.
+  let mut app = SDFBakerApplication::new()?;
+  app.init()?;
+
+  // Run the application.
+  app.run()?;
+
   Ok(())
 }
---
+++ src/config/mod.rs
@@ -0,0 +1,31 @@
+use anyhow::{Result, Context};
+use serde::Deserialize;
+
+mod window;
+
+pub use window::*;
+
+/// The application configure.
+#[derive(Debug, Deserialize, Default, Clone)]
+pub struct AppConfig {
+  pub window: WindowConfig,
+}
+
+/// Validate the application configure.
+/// param: config: the configure.
+/// return: the result of the validation.
+pub fn validate_app_config(config: &AppConfig) -> Result<()> {
+  validate_window_config(&config.window)?;
+  Ok(())
+}
+
+/// Load the application configure.
+/// param: config_file: the configure file path.
+/// return: the application configure.
+pub fn load_app_config(config_file: &str) -> Result<AppConfig> {
+  let config_str = std::fs::read_to_string(config_file)
+    .with_context(|| format!("Failed to read the config file: {}", config_file))?;
+  let config: AppConfig = serde_yaml::from_str(&config_str)
+    .with_context(|| format!("Failed to parse the config file: {}", config_file))?;
+  Ok(config)
+}
