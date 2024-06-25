2a2b6b093b3271e5d85b41211995d5eefc37d18d
zhing2006 <382601573@qq.com>
2024-06-26 00:03:00 +0800

    Update vs.

--- shaders/src/hala-sdf-baker/test.vs_6_8.hlsl
+++ shaders/src/hala-sdf-baker/test.vs_6_8.hlsl
@@ -2,14 +2,16 @@
 #include "test_common.hlsl"
 
 struct vs_in {
-  [[vk::location(0)]] float3 position: POSITION;
-  [[vk::location(1)]] float3 color: COLOR0;
+  [[vk::location(0)]] float4 position: POSITION;
+  [[vk::location(1)]] float4 normal: NORMAL;
+  [[vk::location(2)]] float4 tangent: TANGENT;
+  [[vk::location(3)]] float4 uv: TEXCOORD0;
 };
 
 vs_to_ps main(vs_in input) {
   vs_to_ps output = (vs_to_ps)0;
-  float4 position = mul(m_mtx, float4(input.position, 1.0));
+  float4 position = mul(m_mtx, float4(input.position.xyz, 1.0));
   output.position = mul(vp_mtx, position);
-  output.color = input.color;
+  output.color = input.normal.xyz * 0.5 + 0.5;
   return output;
 }
